4481ad2a7aee259075091f51f5a852c7b14f1920
mississppi <msspstudio@gmail.com>
2020-09-16 22:49:35 +0900

    0916

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          WWWWWelcome toooooo <a href="https://nextjs.org">Next.js!</a>
+          hogehoge:WWWWWelcome toooooo <a href="https://nextjs.org">Next.js!</a>
           hogehogheo
         </h1>
 
